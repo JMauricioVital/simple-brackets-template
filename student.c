f95b370c51f17c4b6b9f99fb529c80c6ebbe3ccf
JMauricioVital <joaocapuchovital@gmail.com>
2025-04-02 08:22:42 -0300

    Update student.c

--- student.c
+++ student.c
@@ -1,23 +1,22 @@
 #include <student.h>
-int simple_parentheses(char *str)
-{
-  int b = 0;
-  // TODO: escreva uma expressao para verificar se a string str esta parentisada corretamente.
-  while (*str)
-  {
-    if (*str == '(')
-    {
-      b++;
-    }
-    else if (*str == ')')
-    {
-      b--;
-      if (b < 0)
-      {
-        return 0;
-      }
-    }
-    str++;
-  }
-  return b;
+int simple_parentheses(char *str) {
+   int b = 0; 
+   char stack[1000]; 
+   int top = -1; 
+   while (*str) {
+       if (*str == '(' || *str == '[' || *str == '{') {
+           stack[++top] = *str; 
+       } else if (*str == ')' || *str == ']' || *str == '}') {
+           if (top < 0) return 0; 
+           char last = stack[top--]; 
+           
+           if ((*str == ')' && last != '(') ||
+               (*str == ']' && last != '[') ||
+               (*str == '}' && last != '{')) {
+               return 0;
+           }
+       }
+       str++;
+   }
+   return top == -1; 
 }
