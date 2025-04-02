5d6b3aceee61f502ef3f77d76d5f3d94facea46f
JMauricioVital <joaocapuchovital@gmail.com>
2025-04-02 08:19:35 -0300

    Update student.c

--- student.c
+++ student.c
@@ -1,7 +1,23 @@
 #include <student.h>
-int simple_parentheses(char * str) {
-  int b;
+int simple_parentheses(char *str)
+{
+  int b = 0;
   // TODO: escreva uma expressao para verificar se a string str esta parentisada corretamente.
-  
+  while (*str)
+  {
+    if (*str == '(')
+    {
+      b++;
+    }
+    else if (*str == ')')
+    {
+      b--;
+      if (b < 0)
+      {
+        return 0;
+      }
+    }
+    str++;
+  }
   return b;
 }
